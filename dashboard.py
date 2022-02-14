b69ae060dc54f8996838263989456487d04051c9
Sergei Kolesnik <kolesniksn2510@gmail.com>
2022-02-14 20:19:35 +0300

    Add info about accuracy

--- dashboard.py
+++ dashboard.py
@@ -7,21 +7,24 @@ from skimage.color import rgb2gray, rgba2rgb
 from skimage.transform import resize
 from keras.datasets import mnist
 from dashboard.plots import plot_example, plot_bar
+from sklearn.metrics import accuracy_score
 
 
 page = st.sidebar.radio("Parts", ["Data", "Recognise handwriting"])
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+with open("nn.pickle", "rb") as nn_file:
+    nn = pickle.load(nn_file)
+
 if page == "Recognise handwriting":
-    image_data = st_canvas(width=300, height=300, stroke_width=36, drawing_mode="freedraw", key="canvas")
+    image_data = st_canvas(width=300, height=300, stroke_width=38, drawing_mode="freedraw", key="canvas")
     if image_data.image_data is not None:
-        with open("nn.pickle", "rb") as nn_file:
-            nn = pickle.load(nn_file)
         digit_gray = 1 - rgb2gray(rgba2rgb(image_data.image_data / 255))
         if st.button("Predict!"):
-            st.write("Is {}".format(np.argmax(nn.predict(resize(digit_gray, (28, 28)).reshape(1, 784)))))
+            st.markdown("Is {}".format(np.argmax(nn.predict(resize(digit_gray, (28, 28)).reshape(1, 784)))))
 elif page == "Data":
-    (train_X, train_y), _ = mnist.load_data()
     st.markdown("## Example of train dataset")
     st.pyplot(plot_example(train_X))
     st.markdown("## Count of each digit")
     st.pyplot(plot_bar(train_y))
+    st.markdown("## Accuracy is _{}_".format(accuracy_score(test_y, np.argmax(nn.predict(test_X.reshape(10000, 784) / 255), axis=1))))
 
