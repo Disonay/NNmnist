a997e00f002600bdb0dc3031bae1f0aa78b1ac66
Sergei Kolesnik <kolesniksn2510@gmail.com>
2022-02-16 00:08:39 +0300

    Create module for data loading

--- dashboard.py
+++ dashboard.py
@@ -1,30 +1,39 @@
-import pickle
+# -*- encoding: utf-8 -*-
 
-import streamlit as st
-from streamlit_drawable_canvas import st_canvas
 import numpy as np
-from skimage.color import rgb2gray, rgba2rgb
-from skimage.transform import resize
-from keras.datasets import mnist
-from dashboard.plots import plot_example, plot_bar
+import streamlit as st
 from sklearn.metrics import accuracy_score
+from streamlit_drawable_canvas import st_canvas
 
+from dashboard.load_data import load_nn, load_train_test_data
+from dashboard.plots import plot_bar, plot_example
+from my_nn.utils import image_preprocessing
+
+(train_X, train_y), (test_X, test_y) = load_train_test_data()
+nn = load_nn("nn.pickle")
 
 page = st.sidebar.radio("Parts", ["Data", "Recognise handwriting"])
-(train_X, train_y), (test_X, test_y) = mnist.load_data()
-with open("nn.pickle", "rb") as nn_file:
-    nn = pickle.load(nn_file)
 
 if page == "Recognise handwriting":
-    image_data = st_canvas(width=300, height=300, stroke_width=38, drawing_mode="freedraw", key="canvas")
+    image_data = st_canvas(
+        width=300, height=300, stroke_width=28, drawing_mode="freedraw", key="canvas"
+    )
     if image_data.image_data is not None:
-        digit_gray = 1 - rgb2gray(rgba2rgb(image_data.image_data / 255))
         if st.button("Predict!"):
-            st.markdown("Is {}".format(np.argmax(nn.predict(resize(digit_gray, (28, 28)).reshape(1, 784)))))
+            st.markdown(
+                "Is {}".format(
+                    np.argmax(
+                        nn.predict(image_preprocessing(image_data.image_data).reshape(1, 784))
+                    )
+                )
+            )
 elif page == "Data":
     st.markdown("## Example of train dataset")
     st.pyplot(plot_example(train_X))
     st.markdown("## Count of each digit")
     st.pyplot(plot_bar(train_y))
-    st.markdown("## Accuracy is _{}_".format(accuracy_score(test_y, np.argmax(nn.predict(test_X.reshape(10000, 784) / 255), axis=1))))
-
+    st.markdown(
+        "## Accuracy is _{}_".format(
+            accuracy_score(test_y, np.argmax(nn.predict(test_X.reshape(10000, 784) / 255), axis=1))
+        )
+    )
