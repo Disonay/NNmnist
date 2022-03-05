270075ff1ff10eaf4d1af0bab09808e0c47657fd
Sergei Kolesnik <kolesniksn2510@gmail.com>
2022-03-05 19:52:31 +0300

    Change layers

--- my_nn/__main__.py
+++ my_nn/__main__.py
@@ -1,11 +1,11 @@
 # -*- encoding: utf-8 -*-
-
+import os
 import pickle
 
 import numpy as np
 from keras.datasets import mnist
 
-from my_nn.layers import Linear, MSECriterion, ReLU, Sigmoid, SoftPlus
+from my_nn.layers import Linear, MSECriterion, ReLU, Sigmoid, SoftPlus, CrossEntropyLoss, SoftMax
 from my_nn.nn import NeuralNet
 from my_nn.utils import kron_delta
 
@@ -15,10 +15,12 @@ nn.add_layer(ReLU())
 nn.add_layer(Linear(128, 64))
 nn.add_layer(ReLU())
 nn.add_layer(Linear(64, 10))
-nn.add_layer(SoftPlus())
-nn.set_loss(MSECriterion())
+nn.add_layer(SoftMax())
+nn.set_loss(CrossEntropyLoss())
 
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+print(os.listdir())
 nn.fit(
     train_X.reshape(60000, 784) / 255,
     np.array(list(map(kron_delta, train_y))).reshape(60000, 10),
